bb13ded858c6af3bccbc7d5af47bc21faab35e1c
libdefi <libcreatesweb3@gmail.com>
2023-07-07 21:56:18 +0900

    Switching WalletConnect from Metamask to Rainbow

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,25 +1,12 @@
 import React, { ReactNode, useContext, useState, createContext } from 'react';
-import { Button, Container, Grid, SvgIcon, Typography, Menu, MenuItem, Box } from '@mui/material';
-import MetaMaskIcon from '../MetaMaskIcon';
 import FullScreenModal from './FullScreenModal';
 import HackathonForm from './HackathonForm';
 import TitleLogo from '../../public/logo.svg';
 
-let injectedProvider = false;
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-if (typeof window.ethereum !== 'undefined') {
-  injectedProvider = true;
-}
-
-const isMetaMask = injectedProvider ? window.ethereum.isMetaMask : false;
-console.log(isMetaMask);
-
-export const walletContext = createContext<any | null>(null);
 
 export const Header = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const [wallet, setWallet] = useState({ accounts: [] });
   const [modalOpen, setModalOpen] = useState(false);
 
   const openModal = () => {
@@ -30,33 +17,6 @@ export const Header = () => {
     setModalOpen(false);
   };
 
-  const updateWallet = async (accounts: any) => {
-    setWallet({ accounts });
-  };
-
-  const handleConnect = async () => {
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
-    });
-    updateWallet(accounts);
-  };
-
-  const handleDisconnect = async () => {
-    await window.ethereum.request({
-      method: 'eth_requestAccounts',
-      params: [{ eth_accounts: {} }],
-    });
-    updateWallet([]);
-    setAnchorEl(null);
-  };
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <div className="navbar bg-primary-content border border-b-gray-300">
@@ -74,35 +34,7 @@ export const Header = () => {
           <FullScreenModal isOpen={modalOpen} onClose={closeModal}>
             <HackathonForm />
           </FullScreenModal>
-          <li className="text-black">
-            {wallet.accounts.length > 0 ? (
-              <>
-                <Button
-                  variant="outlined"
-                  id="basic-button"
-                  aria-controls={open ? 'basic-menu' : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? 'true' : undefined}
-                  onClick={handleClick}
-                  startIcon={<SvgIcon component={MetaMaskIcon} viewBox="0 0 300 300" />}
-                >
-                  {(wallet.accounts[0] as string)?.slice(0, 5)}...
-                  {(wallet.accounts[0] as string)?.slice(-5)}
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button
-                  variant="outlined"
-                  onClick={handleConnect}
-                  startIcon={<SvgIcon component={MetaMaskIcon} viewBox="0 0 300 300" />}
-                  disabled={!isMetaMask}
-                >
-                  Connect
-                </Button>
-              </>
-            )}
-          </li>
+          <ConnectButton />
         </ul>
       </div>
     </div>
