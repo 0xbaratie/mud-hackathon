e80759a3ca73bc4d7aae6c5a2de214304fad1c3b
libdefi <libcreatesweb3@gmail.com>
2023-07-17 15:37:37 +0900

    Unify the colors of the progress bar to the same color

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -36,7 +36,7 @@ export const App = () => {
           {loadingState.msg}
           <br />
           <progress
-            className="mt-6 [&::-webkit-progress-bar]:rounded-sm [&::-webkit-progress-value]:rounded-sm [&::-webkit-progress-bar]:bg-slate-300 [&::-webkit-progress-value]:bg-violet-600 [&::-moz-progress-bar]:bg-violet-600"
+            className="mt-6 [&::-webkit-progress-bar]:rounded-sm [&::-webkit-progress-value]:rounded-sm [&::-webkit-progress-bar]:bg-gray-300 [&::-webkit-progress-value]:bg-black [&::-moz-progress-bar]:bg-black"
             value={Math.floor(loadingState.percentage)}
             max="100"
           />
