d78f94c61dde7918d7a82af43b71c6dc52461dfb
libdefi <libcreatesweb3@gmail.com>
2023-09-10 21:42:53 +0800

    Change form input default to placeholder description

--- packages/client/src/components/HackathonForm.tsx
+++ packages/client/src/components/HackathonForm.tsx
@@ -43,17 +43,17 @@ const HackathonForm: FC<HackathonFormProps> = ({
   const [votingPeriod, setVotingPeriod] = useState(getWeeksLater(3.0));
   const [withdrawalPeriod, setWithdrawalPeriod] = useState(getWeeksLater(4.5));
   const [winnerCount, setWinnerCount] = useState(1);
-  const [name, setName] = useState('Hackathon1');
-  const [uri, setUri] = useState('https://url1');
+  const [name, setName] = useState('');
+  const [uri, setUri] = useState('');
   const [imageUri, setImageUri] = useState(
-    'https://pbs.twimg.com/profile_images/1642968539719163904/xbrZ4_Om_400x400.jpg',
+    '',
   );
-  const [voteNft, setVoteNft] = useState('0xb1008c037aA0dB479B9D5b0E49a27337fB29D72E');
+  const [voteNft, setVoteNft] = useState('');
   const [voteNftSnapshot, setVoteNftSnapshot] = useState(17928076);
 
   return (
     <div className="p-4 overflow-y-auto max-h-[800px]">
-      <h1 className="text-sm mb-1">Hackathon title</h1>
+      <h1 className="text-sm mb-1 font-bold">Title</h1>
       <input
         type="text"
         placeholder="Enter your project title"
@@ -61,7 +61,7 @@ const HackathonForm: FC<HackathonFormProps> = ({
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <h1 className="text-sm mb-1 mt-4">Hackathon detail</h1>
+      <h1 className="text-sm mb-1 mt-4 font-bold">Detail URL</h1>
       <p className="text-sm text-gray-500 mb-1">Please provide details of your hackathon.</p>
       <input
         type="text"
@@ -70,20 +70,20 @@ const HackathonForm: FC<HackathonFormProps> = ({
         value={uri}
         onChange={(e) => setUri(e.target.value)}
       />
-      <h1 className="text-sm mb-1 mt-4">Cover image</h1>
+      <h1 className="text-sm mb-1 mt-4 font-bold">Title image</h1>
       <p className="text-sm text-gray-500 mb-1">
         The ideal aspect ratio is 1 : 1 - for example 512 x 512 px.
       </p>
       <input
         type="text"
         placeholder="http://arweave.net/xxxxxxxxxxxxxx"
-        className="input input-bordered w-full max-w-xs text-gray-900"
+        className="input input-bordered w-full max-w-ms text-gray-900"
         value={imageUri}
         onChange={(e) => setImageUri(e.target.value)}
       />
       <div className="flex">
         <div className="flex-1">
-          <h1 className="text-sm mb-1 mt-4">Hack start datetime (GMT)</h1>
+          <h1 className="text-sm mb-1 mt-4 font-bold">Hack start(GMT)</h1>
           <DateTimePicker
             selectedDateTime={startTimestamp}
             setSelectedDateTime={setStartTimestamp}
@@ -92,7 +92,7 @@ const HackathonForm: FC<HackathonFormProps> = ({
       </div>
       <div className="flex">
         <div className="flex-1">
-          <h1 className="text-sm mb-1 mt-4">Project submit due datetime (GMT)</h1>
+          <h1 className="text-sm mb-1 mt-4 font-bold">Project submit due(GMT)</h1>
           <DateTimePicker 
             selectedDateTime={submitPeriod} 
             setSelectedDateTime={setSubmitPeriod} 
@@ -101,7 +101,7 @@ const HackathonForm: FC<HackathonFormProps> = ({
       </div>
       <div className="flex">
         <div className="flex-1">
-          <h1 className="text-sm mb-1 mt-4">Voting due datetime (GMT)</h1>
+          <h1 className="text-sm mb-1 mt-4 font-bold">Voting due(GMT)</h1>
           <DateTimePicker 
             selectedDateTime={votingPeriod} 
             setSelectedDateTime={setVotingPeriod} 
@@ -110,14 +110,14 @@ const HackathonForm: FC<HackathonFormProps> = ({
       </div>
       <div className="flex">
         <div className="flex-1">
-          <h1 className="text-sm mb-1 mt-4">Withdrawing due datetime (GMT)</h1>
+          <h1 className="text-sm mb-1 mt-4 font-bold">Withdrawing due(GMT)</h1>
           <DateTimePicker
             selectedDateTime={withdrawalPeriod}
             setSelectedDateTime={setWithdrawalPeriod}
           />
         </div>
       </div>
-      <h1 className="text-sm mb-1 mt-4">Number of winners</h1>
+      <h1 className="text-sm mb-1 mt-4 font-bold">Winners</h1>
       <input
         type="number"
         placeholder="1"
@@ -125,7 +125,7 @@ const HackathonForm: FC<HackathonFormProps> = ({
         value={winnerCount}
         onChange={(e) => setWinnerCount(parseFloat(e.target.value))}
       />
-      <h1 className="text-sm mb-1 mt-4 ">Prize token (Optimism chain)</h1>
+      <h1 className="text-sm mb-1 mt-4 font-bold">Prize token (Optimism chain)</h1>
       <select
         className="select select-bordered w-full max-w-xs text-gray-900"
         value={prizeToken}
@@ -135,21 +135,23 @@ const HackathonForm: FC<HackathonFormProps> = ({
         <option value={prizeTokens.USDC}>USDC</option>
         <option value={prizeTokens.DAI}>DAI</option>
       </select>
-      <h1 className="text-sm mb-1 mt-4">Vote NFT Address</h1>
+      <h1 className="text-sm mb-1 mt-4 font-bold">Vote NFT Address</h1>
       <p className="text-sm text-gray-500 mb-1">This vote contract is only ERC721 on L1 only.</p>
       <input
         type="text"
-        className="input input-bordered w-full max-w-xs text-gray-900"
+        placeholder="0xb1008c037aA0dB479B9D5b0E49a27337fB29D72E"
+        className="input input-bordered w-full max-w-s text-gray-900"
         value={voteNft}
         onChange={(e) => setVoteNft(e.target.value)}
       />
-      <h1 className="text-sm mb-1 mt-4">Vote NFT Snapshot</h1>
+      <h1 className="text-sm mb-1 mt-4 font-bold">Vote NFT Snapshot</h1>
       <p className="text-sm text-gray-500 mb-1">
         You need to decide which block ID you want to use to implement the timing of your ownership. Please check the block numbers on <a href="https://etherscan.io/" target="_blank" rel="noopener noreferrer" className="underline">Etherscan</a>.
       </p>
 
       <input
         type="text"
+        placeholder="17928076"
         className="input input-bordered w-full max-w-xs text-gray-900"
         value={voteNftSnapshot}
         onChange={(e) => setVoteNftSnapshot(parseInt(e.target.value))}
